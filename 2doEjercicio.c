f5ff8e1a555109632a079a6affe1811eed37dff8
aech18 <129116406+aech18@users.noreply.github.com>
2023-03-27 20:20:46 -0400

    Delete 2doEjercicio.c

--- 2doEjercicio.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-float main(){
-    float vol,arista;
-    printf("Por favor ingrese el valor de la arista del Cubo: ");
-    scanf("%f", &arista);
-    vol = pow(arista,3);
-    printf ("El volumen del Cubo es %f", vol);
-}
